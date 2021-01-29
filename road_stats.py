11339673d784f07a2de3baaeb5d27964ca05f4a5
Anna Labetski <anna.labetski@gmail.com>
2021-01-29 17:37:52 +0100

    Add road stats script base

---
+++ road_stats.py
@@ -0,0 +1,35 @@
+roadsAreal = ""
+OSM = ""
+carriageways = ""
+
+# Area of convex hull
+# Number of polygons
+# Number of LoD1 edges
+# Polygon to LoD1 edge ratio
+# Polygon area mean + std + median
+# LoD1 edge length mean + stf + median
+# LoD1 edge total length
+# LoD1 edge density (total edge length/area)
+# Area to LoD1 length ratio
+# Road width mean + std + median
+# Number of dual carriageways + proportion of all roads
+# Number of LoD2 edges + percentage increase from OSM
+# LoD2 edges length mean + std + median
+# LoD2 edge total length
+# LoD2 edge density (total edge length/area)
+
+# Number of intersections 
+# Intersection Density
+# Number of edges per node average + std + median + mode
+# Number of 4 dual carriageways
+# Number of 3 duals + 1 single
+# Number of 3 duals
+# Number of 1 dual + 2 singles
+# Number of 2 duals + 1 single
+# Number of dead ends LoD1 + proportion of all nodes
+# Number of dead ends LoD2 + percentage increase + proportion of all nodes
+
+# LoD1 connected components
+# Percentage of edges in largest CC
+# LoD2 CC
+# Percent of edges in largest CC
